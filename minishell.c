6a053a673a7db09d19c8f8ef72456a578d1dda6f
Yunior Moreno <yunioramv@gmail.com>
2024-05-13 00:17:21 -0400

    Se creo el ciclo principal de programa, ciclo de lectura de caracteres y opción de terminar el programa con el comando 'salir'.

--- minishell.c
+++ minishell.c
@@ -3,8 +3,34 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 int main ( int argc, char** argv ){
+
+    //Ciclo principal
+    while ( 1 )
+    {
+        char ch;            // Ultimo caracter leido
+        char cadena[1024];  // Almacena una linea
+        int it = 0;         // Iterador para _cadena_
+
+        printf("--> ");     // Indicador de prompt 
+        
+        //Lectura de caracteres
+        while ( (ch = getchar() ) != '\n' && ch != EOF )
+        {
+            cadena[it] = ch;
+            it++;
+        }
+        // Carccter nulo para indicar fin del string
+        cadena[it + 1] = '\0';
+        //printf("La cadena es: %s\n", cadena);
+
+        if ( ! strcmp(cadena,"salir") ){
+            printf("Adios.\n");
+            exit(0);
+        } exit;
+    }   
 
     return 0;
 }
