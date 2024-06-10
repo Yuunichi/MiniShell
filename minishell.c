c2f6ee2abf6b11fd1855b61e512a2f9bac179eb2
Juan Perez <juanluisperez001@gmail.com>
2024-06-10 00:57:26 -0400

    Ejecucion de comando unico

--- minishell.c
+++ minishell.c
@@ -8,29 +8,44 @@
 int main ( int argc, char** argv ){
 
     //Ciclo principal
-    while ( 1 )
-    {
+    while ( 1 ){     
         char ch;            // Ultimo caracter leido
         char cadena[1024];  // Almacena una linea
+        char path[]="/bin/";
         int it = 0;         // Iterador para _cadena_
-
+        char *arr[10];
         printf("--> ");     // Indicador de prompt 
         
         //Lectura de caracteres
-        while ( (ch = getchar() ) != '\n' && ch != EOF )
-        {
+        while ( (ch = getchar() ) != '\n' && ch != EOF ){
             cadena[it] = ch;
             it++;
         }
-        // Carccter nulo para indicar fin del string
-        cadena[it + 1] = '\0';
-        //printf("La cadena es: %s\n", cadena);
+        cadena[it] = '\0';
+        it=0;
+// Carccter nulo para indicar fin del string
 
         if ( ! strcmp(cadena,"salir") ){
             printf("Adios.\n");
             exit(0);
         } exit;
-    }   
 
-    return 0;
+//Aqui separo en tokens la cadena //
+        char *token = strtok(cadena, " "); 
+        while(token != NULL){
+                arr[it]=token;
+                token = strtok(NULL, " ");
+                it++;
+        }
+        arr[it]=NULL;
+//Aqui termina la separacion de tokens//
+
+//EJECUTANDO EL COMANDO
+        //char *argv2[22]={};
+
+        strcat(path,arr[0]);
+        execvp(path,arr);
+    }
+
+   return 0;
 }
