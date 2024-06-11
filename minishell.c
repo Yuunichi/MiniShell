2b520b52c18d702db584e54e83d2e14faee8f474
Juan Perez <juanluisperez001@gmail.com>
2024-06-10 21:34:02 -0400

    Funciones Lectura y Separar

--- minishell.c
+++ minishell.c
@@ -5,47 +5,84 @@
 #include <unistd.h>
 #include <string.h>
 
+
+void LeerCaracteres(char *cad){
+        int it=0;
+        char ch;            // Ultimo caracter leido
+        while ( (ch = getchar() ) != '\n' && ch != EOF ){
+            cad[it] = ch;
+            it++;
+        }
+        cad[it] = '\0';
+}
+
+void structArr (char* File, char **argv2,char *inst){
+    strcpy(File,"");
+    strcat(File,strtok(inst," "));
+    argv2[0]=File;
+    int i=1;
+    char *iterador;
+    while(i==1 || (i<=20 && iterador != NULL )){
+        iterador = strtok(NULL," ");
+        argv2[i]=iterador;
+        ++i;
+    }
+    argv2[i]=NULL;
+}
+
+
+void separarOR(char *cadena, char **arr){
+        arr[0]=strtok(cadena,"||");
+        int i = 1;
+        char *iterador;
+        while(i==1 || (i<=20 && iterador != NULL )){
+        iterador = strtok(NULL,"||");
+        arr[i]=iterador;
+        ++i;
+    }
+i=0;
+}
+
+
 int main ( int argc, char** argv ){
 
     //Ciclo principal
     while ( 1 ){     
-        char ch;            // Ultimo caracter leido
+
+        char File[30]="";
+        char *argv2[22]={};
         char cadena[1024];  // Almacena una linea
-        char path[]="/bin/";
-        int it = 0;         // Iterador para _cadena_
-        char *arr[10];
+        char *arr[20]={};
         printf("--> ");     // Indicador de prompt 
         
         //Lectura de caracteres
-        while ( (ch = getchar() ) != '\n' && ch != EOF ){
-            cadena[it] = ch;
-            it++;
-        }
-        cadena[it] = '\0';
-        it=0;
-// Carccter nulo para indicar fin del string
+        LeerCaracteres(cadena);
 
         if ( ! strcmp(cadena,"salir") ){
             printf("Adios.\n");
             exit(0);
         } exit;
 
-//Aqui separo en tokens la cadena //
-        char *token = strtok(cadena, " "); 
-        while(token != NULL){
-                arr[it]=token;
-                token = strtok(NULL, " ");
-                it++;
+
+/* ESTA SECCION ESTA EN REVISION */
+        separarOR(cadena, arr);
+        int cont=0;
+        for(int i=0; arr[i]!=NULL;i++){
+                cont++;
         }
-        arr[it]=NULL;
-//Aqui termina la separacion de tokens//
+        if(cont>1){     
+       
+        }
+/* ESTA SECCION ESTA EN REVISION */
 
+        structArr(File, argv2, cadena);
+        int pid = fork();
+            if(!pid){
+                execvp(File,argv2);
+                exit(-1);
+            }
+            wait(NULL);
 //EJECUTANDO EL COMANDO
-        //char *argv2[22]={};
-
-        strcat(path,arr[0]);
-        execvp(path,arr);
     }
-
    return 0;
 }
